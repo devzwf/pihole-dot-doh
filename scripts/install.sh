a9b349c6248661d69cd6b428b786d06a003f0ebf
DevZwF <25246962+devzwf@users.noreply.github.com>
2025-12-31 17:04:08 -0500

    replace cloudfrared-dns-proxy  with dnscrypt-proxy

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,35 +3,12 @@
 # Let Download and install Cloudflare depending on the architecture
 set -eux; 
 ARCH="$(apk --print-arch | awk -F'-' '{print $NF}')"
-case "$ARCH" in
-    aarch64|arm64)
-        CF_PACKAGE="cloudflared-linux-arm64"
-        ;;
-    arm|armv7)
-        CF_PACKAGE="cloudflared-linux-arm"
-        ;;
-    armhf)
-        CF_PACKAGE="cloudflared-linux-armhf"
-        ;;
-    amd64|x86_64)
-        CF_PACKAGE="cloudflared-linux-amd64"
-        ;;
-    *)
-        echo "Unsupported architecture: $ARCH"
-        exit 1;
-        ;;
-esac;
-
-# install cloudflared
-wget -q https://github.com/cloudflare/cloudflared/releases/latest/download/${CF_PACKAGE} \
-    -O /usr/local/bin/cloudflared && \
-    chmod +x /usr/local/bin/cloudflared \
-&& echo "$(date "+%d.%m.%Y %T") $(cloudflared -V) installed for ${ARCH}" >> /build_date.info
 
 
-useradd -s /usr/sbin/nologin -r -M cloudflared \
-    && chown cloudflared:cloudflared /usr/local/bin/
-    
+# install dnscrypt-proxy
+apk add --update --no-cache dnscrypt-proxy \
+    && echo "$(date "+%d.%m.%Y %T") DNSCrypt-proxy $(dnscrypt-proxy -version) installed for ${ARCH}" >> /build_date.info
+
 # install stubby
 apk add --update --no-cache --repository http://dl-cdn.alpinelinux.org/alpine/edge/community/ stubby \
     && echo "$(date "+%d.%m.%Y %T") $(stubby -V) installed for ${ARCH}" >> /build_date.info
@@ -39,14 +16,7 @@ apk add --update --no-cache --repository http://dl-cdn.alpinelinux.org/alpine/ed
 # install unbound 
 apk add --update --no-cache --repository http://dl-cdn.alpinelinux.org/alpine/edge/main/ unbound \
     && echo "$(date "+%d.%m.%Y %T") Unbound $(unbound -V | head -1) installed for ${ARCH}" >> /build_date.info
-
-# clean cloudflared config
-mkdir -p /etc/cloudflared \
-    && rm -f /etc/cloudflared/config.yml
-    
-# add unbound version to build.info
-#echo "$(date "+%d.%m.%Y %T") Unbound $(/usr/local/sbin/unbound -V | head -1) installed for ${ARCH}" >> /build_date.info    
-
+ 
 # add pihole version to build.info
 echo "$(date "+%d.%m.%Y %T")  $(/usr/local/bin/pihole -v)"
 echo "$(date "+%d.%m.%Y %T")  $(/usr/local/bin/pihole -v |sed -n '1p') installed" >> /build_date.info
