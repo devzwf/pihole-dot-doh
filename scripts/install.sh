5e55df72ae733a90f601a830e0cc03415d355651
DevZwF <25246962+devzwf@users.noreply.github.com>
2026-02-08 14:48:26 -0500

    align the check and install with alpine instead of github

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,26 +1,32 @@
 #!/bin/bash
-
-# Let Download and install Cloudflare depending on the architecture
 set -eux; 
+
+# Get architecture
 ARCH="$(apk --print-arch | awk -F'-' '{print $NF}')"
 
-
-# install dnscrypt-proxy
-apk add --update --no-cache dnscrypt-proxy \
+# 1. Install dnscrypt-proxy from edge community
+apk add --update --no-cache \
+    --repository http://dl-cdn.alpinelinux.org/alpine/edge/community/ \
+    dnscrypt-proxy \
     && echo "$(date "+%d.%m.%Y %T") DNSCrypt-proxy $(dnscrypt-proxy -version) installed for ${ARCH}" >> /build_date.info
 
-# install stubby
-apk add --update --no-cache --repository http://dl-cdn.alpinelinux.org/alpine/edge/community/ stubby \
+# 2. Install stubby from edge community
+apk add --update --no-cache \
+    --repository http://dl-cdn.alpinelinux.org/alpine/edge/community/ \
+    stubby \
     && echo "$(date "+%d.%m.%Y %T") $(stubby -V) installed for ${ARCH}" >> /build_date.info
 
-# install unbound 
-apk add --update --no-cache --repository http://dl-cdn.alpinelinux.org/alpine/edge/main/ unbound \
+# 3. Install unbound from edge main
+apk add --update --no-cache \
+    --repository http://dl-cdn.alpinelinux.org/alpine/edge/main/ \
+    unbound \
     && echo "$(date "+%d.%m.%Y %T") Unbound $(unbound -V | head -1) installed for ${ARCH}" >> /build_date.info
- 
-# add pihole version to build.info
+
+# 4. Pi-hole version logging
 echo "$(date "+%d.%m.%Y %T")  $(/usr/local/bin/pihole -v)"
-echo "$(date "+%d.%m.%Y %T")  $(/usr/local/bin/pihole -v |sed -n '1p') installed" >> /build_date.info
-echo "$(date "+%d.%m.%Y %T")  $(/usr/local/bin/pihole -v |sed -n '2p') installed" >> /build_date.info
-echo "$(date "+%d.%m.%Y %T")  $(/usr/local/bin/pihole -v |sed -n '3p') installed" >> /build_date.info
-# clean up
+/usr/local/bin/pihole -v | sed -n '1,3p' | while read -r line; do
+    echo "$(date "+%d.%m.%Y %T") $line installed" >> /build_date.info
+done
+
+# Clean up
 rm -rf /tmp/* /var/tmp/*
